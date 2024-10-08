666f2539e3b2f8efbe70f2dd625282f8757227fa
Abdul Majeed <johnharris5934@gmail.com>
2024-10-08 18:27:17 +0500

    first commit

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ setInterval(() => {
 
   loo++
 
-  if (loo == 1) {
+  if (loo === 1) {
 
     img.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(https://images.adsttc.com/media/images/59bc/8700/b22e/38ff/0100/0425/newsletter/10-1.jpg?1505527542)'
   }
-  if (loo == 2) {
+  if (loo === 2) {
 
     img.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(bg.jpg)`
 
   }
 
-  if (loo == 3) {
+  if (loo === 3) {
 
     img.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(four (1).jpg)`
 
   }
-  if( loo == 4 ){
+  if( loo === 4 ){
 
      img.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(five.jpg)`
   }
